507e6942668dfb803d659a67f79054b5b69c316a
NaySurGithub <96527285+NaySurGithub@users.noreply.github.com>
2026-08-18 22:38:15 +0200

    feat: implemented seagrass block (#43)
    * Implemented seagrass
    
    * fix: address seagrass review feedback
    
    ---------
    
    Co-authored-by: NaySurGithub <NaySurGithub@users.noreply.github.com>
    Co-authored-by: xRookieFight <me@xrookiefight.com>

--- src/block/BlockTypeIds.php
+++ src/block/BlockTypeIds.php
@@ -856,8 +856,9 @@ final class BlockTypeIds{
 	public const FLOWERING_AZALEA = 10826;
 
 	public const DECORATED_POT = 10827;
+	public const SEAGRASS = 10828;
 
-	public const FIRST_UNUSED_BLOCK_ID = 10828;
+	public const FIRST_UNUSED_BLOCK_ID = 10829;
 
 	private static int $nextDynamicId = self::FIRST_UNUSED_BLOCK_ID;
 
